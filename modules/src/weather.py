7860df27ad04cd2a4dd8d3952c9166aa87e983a6
Junxiang Ji <dashjim@gmail.com>
2018-07-18 00:58:10 -0700

    enable the weather forcast ablity.

--- modules/src/weather.py
+++ modules/src/weather.py
@@ -12,20 +12,15 @@ OPEN_WEATHER_MAP_ACCESS_TOKEN = os.environ.get('OPEN_WEATHER_MAP_ACCESS_TOKEN', 
 def process(input, entities):
     output = {}
     try:
-        r = requests.get(
-            'https://maps.googleapis.com/maps/api/geocode/json?address=' + entities['weather_location'][0][
-                'value'] + '&key=' + GOOGLE_MAPS_API_KEY)
-        location_data = r.json()
         r = requests.get('http://api.openweathermap.org/data/2.5/weather?lat=' + str(
-            location_data['results'][0]['geometry']['location']['lat']) + '&lon=' + str(
-            location_data['results'][0]['geometry']['location'][
-                'lng']) + '&units=metric&appid=' + OPEN_WEATHER_MAP_ACCESS_TOKEN)
+            entities['location'][0]['resolved']['values'][0]['coords']['lat']) + '&lon=' + str(
+            entities['location'][0]['resolved']['values'][0]['coords']['long']) + '&units=metric&appid=' + OPEN_WEATHER_MAP_ACCESS_TOKEN)
         weather_data = r.json()
         output['input'] = input
         temperature_in_fahrenheit = weather_data['main']['temp'] * 1.8 + 32
         degree_sign = u'\N{DEGREE SIGN}'
         output['output'] = TextTemplate(
-            'Location: ' + location_data['results'][0]['formatted_address'] + '\nWeather: ' +
+            'Location: ' + entities['location'][0]['resolved']['values'][0]['name'] + '\nWeather: ' +
             weather_data['weather'][0][
                 'description'] + '\nTemperature: ' + str(
                 weather_data['main']['temp']) + ' ' + degree_sign + 'C / ' + str(
